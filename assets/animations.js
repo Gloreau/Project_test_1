e7bfe14bfc13b7a5f39bf6853d5cbcea982aaa54
Gloreau <loreau.g@gmail.com>
2020-12-15 23:09:44 +0100

    Hero + slidercontainer

--- assets/animations.js
+++ assets/animations.js
@@ -1,4 +1,7 @@
 import { gsap } from "gsap";
+import { CSSRulePlugin } from "gsap/CSSRulePlugin";
+gsap.registerPlugin(CSSRulePlugin);
+
 
 // Hompage Timeline 
 
@@ -15,10 +18,32 @@ tlhome.from ('.navbar-nav', {
     ease:"power1. out",
    
 });
+
 tlhome.from ('.infos', {
     opacity:0,
     duration:0.5,
     ease:"power1. out",
+   
+});
+tlhome.from ('.hero', {
+    opacity:0,
+    duration:0.5,
+    ease:"power1. out",
+   
+},0.5);
+tlhome.from ('.hero__ctn', {
+    opacity:0,
+    y:'-100%',
+    duration:0.3,
+    ease:"power1. out",
+   
+});
+tlhome.from ('.hero__horaires__ctn', {
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    ease:"power3. out",
+    stagger:0.4,
    
 });
 
@@ -31,6 +56,6 @@ gsap.to('.hideh1', {
     delay:1,
     stagger:0.5
 
-})
+});
 
 
